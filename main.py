0c23e7d5f808db1abf414ac34d7a8228dbf1ab5c
manu6287 <shreyanshjha65@gmail.com>
2025-02-02 20:03:09 +0100

    advanced chatbot logic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
+import sys
 import os
-from flask import Flask, render_template, request, url_for, session
+from flask import Flask, jsonify, render_template, request, url_for, session
 import google.generativeai as genai
 from dotenv import load_dotenv
 import uuid
@@ -7,7 +9,8 @@ import base64
 import json
 import enum
 from typing_extensions import TypedDict
-from structures import Type, Analysis, FeedbackArtStyle, FeedbackEmotionEval, FeedbackIntent, FeedbackSthSpecific
+from structures import *
+
 
 load_dotenv()
 app = Flask(__name__)
@@ -20,7 +23,7 @@ genai.configure(api_key=api_key)
 if not api_key:
     raise ValueError("Please make sure to define your GOOGLE_API_KEY in your .env file")
     
-def response(prompt, instruction, schema):
+def json_response(prompt, instruction, schema):
     try:
         result = genai.GenerativeModel("gemini-1.5-flash", system_instruction=instruction).generate_content(
             [prompt],
@@ -34,7 +37,7 @@ def response(prompt, instruction, schema):
     except Exception as e:
         return {"error": str(e)}
     
-def response_with_image(prompt, instruction, schema, image_path):
+def json_response_with_image(prompt, instruction, schema, image_path):
     
     try:
         # Load the image using Pillow
@@ -55,51 +58,53 @@ def response_with_image(prompt, instruction, schema, image_path):
     except Exception as e:
         return {"error": str(e)}
     
-def feedback_intent_resolution (prompt: str):
+def feedback_intent_resolution(prompt: str):
     try:
-
-        instruction = "Determine which aspect of their art the user intends to work on based on the given prompt. Reply only with one of the following enums for FeedbackIntent: 'Artstyle', 'Emotion', or 'Specific Object'."
-
+        instruction = "Please evaluate carefully whether the given image is of a painting(made with paint) or a sketch(made with pencil) and return the enum value that matches according to the specified response schema."
         # Generate Response
-        result = response(prompt=prompt, instruction=instruction, schema=Type) 
+        result = json_response(prompt=prompt, instruction=instruction, schema=FeedbackType)
         
-        # Extract JSON response (assuming the response is inside `candidates[0]['content']['parts'][0]['text']`)
-        response_text = result.candidates[0].content.parts[0].text.strip()
-        response_dict = json.loads(response_text)
+        if "error" in result:
+            print(f"Error in LLM response: {result['error']}", file=sys.stderr)
+            return None, None
+        
+        try:
+            response_text = result.candidates[0].content.parts[0].text.strip()
+            
+            response_dict = json.loads(response_text)
+            intent = FeedbackType(response_dict)
+            print(f"Feedback Type: {intent}", file=sys.stderr)
+        except (json.JSONDecodeError, KeyError, IndexError) as e:
+            print(f"Error parsing JSON: {e}", file=sys.stderr)
+            return None, None
+        
 
-        # Extract the FeedbackIntent value
-        intent_value = response_dict.get("FeedbackIntent")
-
-        # Convert to Enum
-        intent = FeedbackIntent(intent_value)
-
-        # Map the response to the Enum (Ensure it matches exactly)
         schema = ''
-        # Execute Different Code Based on the Intent
-        if intent == FeedbackIntent.ARTSTYLE:
-            instruction = 'Give feedback on how to improve the given artwork for the desired artstyle for each category in the schema. Reply with the specified json schema.'
-            schema = FeedbackArtStyle
+        instruction = "You are an app which compares how similar questions are. Compare the prompt to all the enum values and return the most similar one while following the response schema."
+        if intent == FeedbackType.PAINTING:
+            result = json_response(prompt=prompt, instruction=instruction, schema=PaintingIntent)
+            response_text = result.candidates[0].content.parts[0].text.strip()
+            response_dict = json.loads(response_text)
+            intent = PaintingIntent(response_dict)
+            instruction = 'You are a chatbot that provides detailed feedback on the user\'s painting for each field in the json response schema. The tone should be soft and educational, providing concrete suggestions for improvement.'
+        elif intent == FeedbackType.SKETCH:
+            result = json_response(prompt=prompt, instruction=instruction, schema=SketchIntent)
+            response_text = result.candidates[0].content.parts[0].text.strip()
+            response_dict = json.loads(response_text)
+            intent = SketchIntent(response_dict)
+            instruction = 'You are a chatbot that provides detailed feedback on the user\'s sketch for each field in the json response schema. The tone should be soft and educational, providing concrete suggestions for improvement.'
 
-            print("User wants to improve their art style. Suggesting relevant techniques...")
-            # Execute Code for Art Style Enhancement
-        elif intent == FeedbackIntent.EMOTION:
-            instruction = 'Give feedback on how to convey or better convey the desired emotion in the given artwork for each category in the schema. Reply with the specified json schema.'
-            schema = FeedbackEmotionEval
-            print("User wants to convey emotions better in their art. Suggesting emotional expression techniques...")
-            # Execute Code for Enhancing Emotion in Art
-        elif intent == FeedbackIntent.SPECIFIC_OBJECT:
-            instruction = 'Give feedback on how to add or better include the specific desired object(s) into the given artwork for each category in the schema. Reply with the specified json schema.'
-            schema = FeedbackSthSpecific
-            print("User wants to focus on a specific object. Providing detailed object-based suggestions...")
-            # Execute Code for Improving Specific Object Representation
         else:
-            print("Unknown intent detected.")
+            return {"error": "Please specify whether you are asking for feedback on a sketch or a painting."}
+        print(f"Feedback Intent: {intent}", file=sys.stderr)
+        schema = resolve_schema(intent)
 
         return instruction, schema
 
     except Exception as e:
-        return {"error": str(e)}
-
+        print(f"Error in feedback_intent_resolution: {e}", file=sys.stderr)
+        return None, None
+    
 def analyze_image(image_path):
     try:
         instruction = (
@@ -113,7 +118,7 @@ def analyze_image(image_path):
 
         prompt = "What can you tell me about this work of art?"
 
-        result = response_with_image(prompt=prompt, instruction=instruction, schema=Analysis, image_path=image_path)
+        result = json_response_with_image(prompt=prompt, instruction=instruction, schema=Analysis, image_path=image_path)
         
         # Extract the content from the result
         if result and hasattr(result, 'candidates') and result.candidates:
@@ -132,44 +137,97 @@ def analyze_image(image_path):
             
 
 # Function to Generate Chatbot Response
-def generate_chatbot_response(image_path, user_prompt):
+def generate_chatbot_response(image_path, prompt, schema, instruction):
+     try:
+         # Load the image using Pillow
+         if image_path:
+             with open(image_path, "rb") as image_file:
+                 image_data = image_file.read()
+             base64_image = base64.b64encode(image_data).decode('utf-8')
+         else:
+             base64_image = None
+
+         chat_history = session.get('chat_history', [])
+
+         if base64_image:
+             response = genai.GenerativeModel("gemini-1.5-flash", system_instruction=instruction).generate_content(
+                 [{'mime_type': 'image/jpeg', 'data': base64_image}, prompt] + chat_history,
+                 generation_config=genai.GenerationConfig(
+                 response_mime_type="application/json",
+                 response_schema=schema)
+                 )
+         else:
+             response = genai.GenerativeModel("gemini-1.5-flash", system_instruction=instruction).generate_content(
+                 [prompt] + chat_history,
+                 generation_config=genai.GenerationConfig(
+                 response_mime_type="application/json",
+                 response_schema=schema)
+                 )
+         chat_history.append(f'User: {prompt} Bot: {response.text}')
+         session['chat_history'] = chat_history
+         return response
+     except Exception as e:
+         return {"error": str(e)}
+    
+def chatbot_response_process(prompt, image_path):
     try:
-        # Load the image using Pillow
-        if image_path:
-            with open(image_path, "rb") as image_file:
-                image_data = image_file.read()
-            base64_image = base64.b64encode(image_data).decode('utf-8')
-        else:
-            base64_image = None
-
         chat_history = session.get('chat_history', [])
-        prompt = f"""
-           You are a chatbot designed to give feedback on drawings and paintings.
-           Your goal is to provide constructive and helpful suggestions that will help improve user artwork.
+        instruction = None
+        schema = None
+        if chat_history.__len__() < 1:
+            instruction, schema = feedback_intent_resolution(prompt)
 
-           Your tone should be positive and encouraging. 
-           If no image is provided in the prompt then just respond to the prompt alone.
-           If an image is provided respond to the prompt based on the image.
+        if instruction is None or schema is None and image_path is None:
+            instruction = 'You are a chatbot that provides detailed feedback only on the requested specific factor of the user\'s art. The tone should be soft and educational, providing concrete suggestions for improvement. Use 200 words or less.'
+            schema=Reply
 
-           User Prompt: {user_prompt}
-
-           Chat History: {chat_history}
-       """
-
-        if base64_image:
-            response = genai.GenerativeModel("gemini-1.5-flash").generate_content(
-                [{'mime_type': 'image/jpeg', 'data': base64_image}, prompt]
-            )
+        if image_path:
+            result = generate_chatbot_response(prompt=prompt, instruction=instruction, schema=schema, image_path=image_path)
         else:
-            response = genai.GenerativeModel("gemini-1.5-flash").generate_content(prompt)
+            result = generate_chatbot_response(prompt=prompt, instruction=instruction, schema=schema)
 
-        chat_history.append(f'User: {user_prompt} Bot: {response.text}')
-        session['chat_history'] = chat_history
-        return response.text
+        if "error" in result:
+             print(f"ERROR: LLM Response Error: {result['error']}", file=sys.stderr)
+             return {"error": "ERROR: Failed to generate chatbot response"}
+        try:
+             response_text = result.candidates[0].content.parts[0].text.strip()
+             response_dict = json.loads(response_text)
+        except (json.JSONDecodeError, KeyError, IndexError) as e:
+             print(f"ERROR: JSON Parsing Error: {e}", file=sys.stderr)
+             return {"error": "ERROR: Failed to parse JSON response"}
+
+        return response_dict
     except Exception as e:
-        return f"Error: {str(e)}"
-
-
+        print(f"ERROR: chatbot_response_process failed: {e}", file=sys.stderr)
+        return {"error": "ERROR: chatbot_response_process failed"}
+     
+def resolve_schema(intent):
+    intent_to_template = {
+        PaintingIntent.A: ResponseTemplateA,
+        PaintingIntent.B: ResponseTemplateB,
+        PaintingIntent.C: ResponseTemplateC,
+        PaintingIntent.D: ResponseTemplateD,
+        PaintingIntent.E: ResponseTemplateE,
+        PaintingIntent.F: ResponseTemplateF,
+        PaintingIntent.G: ResponseTemplateG,
+        PaintingIntent.H: ResponseTemplateH,
+        PaintingIntent.I: ResponseTemplateI,
+        PaintingIntent.J: ResponseTemplateJ,
+        SketchIntent.K: ResponseTemplate1,
+        SketchIntent.L: ResponseTemplate2,
+        SketchIntent.M: ResponseTemplate3,
+        SketchIntent.N: ResponseTemplate4,
+        SketchIntent.O: ResponseTemplate5,
+        SketchIntent.P: ResponseTemplate6,
+        SketchIntent.Q: ResponseTemplate7,
+        SketchIntent.R: ResponseTemplate8,
+        SketchIntent.S: ResponseTemplate9,
+        SketchIntent.T: ResponseTemplate10,
+        SketchIntent.U: ResponseTemplate11,
+    }
+    
+    return intent_to_template.get(intent, None)
+     
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -198,21 +256,22 @@ def index():
 def chat():
     if 'file' not in request.files:
         user_prompt = request.form.get('msg')
-        chat_response = generate_chatbot_response(None, user_prompt)
+        
+        chat_response = jsonify(chatbot_response_process(prompt=user_prompt))
         return chat_response
 
     file = request.files['file']
     user_prompt = request.form.get('msg')
 
     if file.filename == '':
-        chat_response = generate_chatbot_response(None, user_prompt)
+        chat_response = jsonify(chatbot_response_process(prompt=user_prompt))
         return chat_response
 
     if file:
         filename = str(uuid.uuid4()) + os.path.splitext(file.filename)[1]
         filepath = os.path.join('static', 'uploads', filename)
         file.save(filepath)
-        chat_response = generate_chatbot_response(filepath, user_prompt)
+        chat_response = jsonify(chatbot_response_process(image_path=filepath, prompt=user_prompt))
         return chat_response
 
 
--- structures.py
+++ structures.py
@@ -7,6 +7,10 @@ class Type(enum.Enum):
     BUILDING = "Building"
     SCULPTURE = "Sculpture"
     ARTWORK = "Artwork"
+    
+class FeedbackType(enum.Enum):
+    PAINTING = "Painting"
+    SKETCH = "Sketch"
 
 
 class Analysis(TypedDict):
@@ -37,7 +41,7 @@ class FeedbackEmotionEval(TypedDict):
     scale_proportions: str
     brushstroke_mov: str
 
-class FeedbackSthSpecific:
+class FeedbackSthSpecific(TypedDict):
     prop_anatomy: str
     persp_depth: str
     line_quality: str
@@ -53,3 +57,199 @@ class FeedbackIntent(enum.Enum):
     ARTSTYLE = "Artstyle"
     EMOTION = "Emotion"
     SPECIFIC_OBJECT = "Specific Object" 
+    
+class Reply(TypedDict):
+    response: str
+    
+import enum
+
+class SketchIntent(enum.Enum):
+    K = "How to improve a sketch?"
+    L = "How to make a sketch more in a particular style?"
+    M = "How to express particular emotions in a sketch?"
+    N = "What’s the story behind my sketch?"
+    O = "How to improve the proportions of a sketch?"
+    P = "How to add more detail to a sketch?"
+    Q = "How to enhance depth in a sketch?"
+    R = "How to simplify a sketch for clarity?"
+    S = "How to improve the fluidity of my sketch?"
+    T = "How to make a sketch feel more energetic?"
+    U = "How to balance light and dark in a sketch?"
+class PaintingIntent(enum.Enum):
+    A = "How to improve this artwork?"
+    B = "How to make a painting more in a particular style?"
+    C = "How to express particular emotions in a painting?"
+    D = "What’s the story behind my painting?"
+    E = "How to enhance the depth of a painting?"
+    F = "How can I add more movement to my painting?"
+    G = "How to simplify a complex painting?"
+    H = "How do I make my painting more dynamic?"
+    I = "How to make a painting look more realistic?"
+    J = "How to adjust a painting’s composition for balance?"
+    
+class ResponseTemplate1(TypedDict):
+    line_work: str
+    shading_and_value: str
+    proportions_and_anatomy: str
+    composition: str
+    texture_and_detail: str
+    perspective: str
+    light_and_shadow: str
+    final_polish: str
+
+class ResponseTemplate2(TypedDict):
+    line_work: str
+    shading_and_value: str
+    proportions_and_anatomy: str
+    composition: str
+    emotion_and_expression: str
+    gesture_and_body_language: str
+    style: str
+
+class ResponseTemplate3(TypedDict):
+    line_work: str
+    shading_and_value: str
+    composition: str
+    emotion_and_expression: str
+    body_language_and_gesture: str
+    light_and_shadow: str
+
+class ResponseTemplate4(TypedDict):
+    subject_matter: str
+    line_work: str
+    body_language_and_gesture: str
+    composition: str
+    emotion_and_expression: str
+    symbolism: str
+    light_and_shadow: str
+    context_and_background: str
+
+class ResponseTemplate5(TypedDict):
+    proportions_and_anatomy: str
+    line_work: str
+    perspective: str
+    composition: str
+    proportional_adjustments: str
+
+class ResponseTemplate6(TypedDict):
+    texture_and_detail: str
+    shading_and_value: str
+    line_work: str
+    emotion_and_expression: str
+
+class ResponseTemplate7(TypedDict):
+    shading_and_value: str
+    perspective: str
+    light_and_shadow: str
+    proportions_and_anatomy: str
+    composition: str
+
+class ResponseTemplate8(TypedDict):
+    line_work: str
+    negative_space: str
+    composition: str
+    shading_and_value: str
+    clarity_and_cleanliness: str
+
+class ResponseTemplate9(TypedDict):
+    line_work: str
+    gesture_and_body_language: str
+    composition: str
+    emotion_and_expression: str
+
+class ResponseTemplate10(TypedDict):
+    line_work: str
+    movement_and_gesture: str
+    composition: str
+    emotion_and_expression: str
+        
+class ResponseTemplate11(TypedDict):
+    shading_and_value: str
+    light_and_shadow: str
+    contrast: str
+    composition: str
+class ResponseTemplateA(TypedDict): 
+    color_palette: str 
+    shading_and_contrast: str 
+    line_quality: str 
+    texture: str 
+    composition: str 
+    light_and_shadow: str
+    subject_matter: str
+    proportions_and_anatomy: str
+    final_polish: str
+
+class ResponseTemplateB(TypedDict): 
+    brushwork: str
+    color_palette: str
+    line_quality: str
+    texture: str
+    composition: str
+    subject_matter: str
+    emotion_and_expression: str
+    light_and_shadow: str
+    style: str
+
+class ResponseTemplateC(TypedDict): 
+    color_palette: str
+    brushwork: str
+    composition: str
+    light_and_shadow: str
+    texture: str
+    line_quality: str
+    emotion_and_expression: str
+    proportions_and_anatomy: str
+
+class ResponseTemplateD(TypedDict): 
+    subject_matter: str
+    composition: str
+    line_work: str
+    symbolism: str
+    color_palette: str
+    light_and_shadow: str
+    emotion_and_expression: str
+    context_and_background: str
+    proportions_and_anatomy: str
+
+class ResponseTemplateE(TypedDict): 
+    shading_and_contrast: str
+    light_and_shadow: str
+    proportions_and_anatomy: str
+    composition: str
+    texture: str
+
+class ResponseTemplateF(TypedDict): 
+    brushwork: str
+    composition: str
+    emotion_and_expression: str
+    line_quality: str
+    subject_matter: str
+
+class ResponseTemplateG(TypedDict): 
+    composition: str
+    negative_space: str
+    proportions_and_anatomy: str
+    final_polish: str
+    subject_matter: str
+
+class ResponseTemplateH(TypedDict): 
+    brushwork: str
+    color_palette: str
+    line_quality: str
+    light_and_shadow: str
+    composition: str
+    movement_and_gesture: str
+
+class ResponseTemplateI(TypedDict): 
+    shading_and_contrast: str
+    proportions_and_anatomy: str
+    texture: str
+    light_and_shadow: str
+    composition: str
+
+class ResponseTemplateJ(TypedDict): 
+    composition: str
+    negative_space: str
+    proportions_and_anatomy: str
+    subject_matter: str
+    light_and_shadow: str
